60320985aa0c7931c557de31c86decebb16dfa88
Rampagerushtc <rampagerushtc@gmail.com>
2019-09-04 19:45:05 +0500

    added toggle random planet button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from '../header';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 import RandomPlanet from '../random-planet';
+import './app.css';
 
 //import PlanetDetails from '../planet-details';
-const App = () => {
-    
-    return (
-        <div>
-            <Header />
-            <RandomPlanet />
 
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList />
-                </div>
-            </div>
-            <div className="col-md-6">
-                <PersonDetails />
-            </div>
+export default class App extends Component {
+    state = {
+        showRandomPlanet: true
+    }
 
-        </div>
-        
-    )
+toggleRandomPlanet = () => {
+    this.setState((state)=>{
+        return {
+            showRandomPlanet: !state.showRandomPlanet
+        }
+    });
 }
 
-export default App;
+
+
+    render() {
+        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+        return (
+            <div>
+                <Header />
+                {planet}
+                <button className="toggle-planet btn btn-warning"
+                        onClick={this.toggleRandomPlanet}>
+                    Toggle Random Planet
+                    </button>
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList />
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails />
+                </div>
+    
+            </div>
+        )
+    }
+    
+}
