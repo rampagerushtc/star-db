822f69355da97d732875536768ada87aafc87c1b
Rampagerushtc <rampagerushtc@gmail.com>
2019-10-17 18:40:49 +0500

    switch to page not found

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,7 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
@@ -63,35 +63,32 @@ export default class App extends Component {
                             <Header onServiceChange={this.onServiceChange} />
                             <RandomPlanet />
 
-                            <Route path="/" render={() => <h2>Welcome to StarDB</h2>}
-                                exact />
-                            <Route path="/people" render={() => <h2>People</h2>}
-                                exact />
-                            <Route path="/people/:id?" component={PeoplePage} />
-                            <Route path="/planets" render={() => <h2>Planets</h2>}
-                                exact />
-                            <Route path="/planets" component={PlanetsPage} />
-                            <Route path="/starships" exact render={() => <h2>Starships</h2>}
-                                exact />
-                            <Route path="/starships" exact component={StarshipsPage} />
-                            <Route path="/starships/:id"
-                                render={({ match }) => {
-                                    const { id } = match.params;
-                                    return <StarshipDetails itemId={id} />
-                                }
-                                } />
-                            <Route
-                                path="/login"
-                                render={() => (
-                                    <LoginPage
-                                        isLoggedIn={isLoggedIn}
-                                        onLogin={this.onLogin} />
-                                )} />
-                            <Route
-                                path="/secret"
-                                render={() => (
-                                    <SecretPage isLoggedIn={isLoggedIn} />
-                                )} />
+                            <Switch>
+                                <Route path="/" render={() => <h2>Welcome to StarDB</h2>}
+                                    exact />
+                                <Route path="/people/:id?" component={PeoplePage} />
+                                <Route path="/planets" component={PlanetsPage} />
+                                <Route path="/starships" exact component={StarshipsPage} />
+                                <Route path="/starships/:id"
+                                    render={({ match }) => {
+                                        const { id } = match.params;
+                                        return <StarshipDetails itemId={id} />
+                                    }
+                                    } />
+                                <Route
+                                    path="/login"
+                                    render={() => (
+                                        <LoginPage
+                                            isLoggedIn={isLoggedIn}
+                                            onLogin={this.onLogin} />
+                                    )} />
+                                <Route
+                                    path="/secret"
+                                    render={() => (
+                                        <SecretPage isLoggedIn={isLoggedIn} />
+                                    )} />
+                                <Route render={() => <h2>Page not found</h2>} />
+                            </Switch>
                         </div>
                     </Router>
                 </SwapiServiceProvider>
